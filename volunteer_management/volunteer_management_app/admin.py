983e641b7974c364aba72bc0fe3e0153363429cf
NotNighter <notnighter@gmail.com>
2024-05-09 19:34:37 +0700

    update admin.py

--- volunteer_management/volunteer_management_app/admin.py
+++ volunteer_management/volunteer_management_app/admin.py
@@ -1,10 +1,76 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from .forms import *
+
+def staff_users(self, obj):
+    User = get_user_model()
+    return User.objects.filter(is_staff=True)
+
+def is_event_organizer(user, event):
+    if not user.is_authenticated:
+        return False
+    try:
+        custom_user = user.customuser
+        return custom_user == event.organizer
+    except User.customuser.RelatedObjectDoesNotExist:
+        return False
+
+class VolunteerEventPostAdmin(admin.ModelAdmin):
+    list_display = ['name', 'start_date', 'end_date', 'location', 'hours', 'organizer', 'status']
+    list_filter = ['start_date']
+    search_fields = ['name', 'location', 'description']
+    ordering = ['-start_date']
+    search_fields = ['name', 'location', 'description']
+    
+    def staff_users(self, obj):
+        User = get_user_model()
+        return User.objects.filter(is_staff=True)
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['organizer'].queryset = staff_users(self, obj)
+        return form
+    
+    fieldsets = (
+        ('Thông tin sự kiện', {'fields': ('name', 'start_date', 'end_date', 'location', 'description', 'hours', 'organizer', 'status')}),
+    )
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ['id', 'username', 'name', 'age', 'email', 'phone', 'hours_worked', 'is_staff', 'is_active']
-    list_filter = ['is_staff', 'is_active']
+    list_display = ['username', 'name', 'age', 'email', 'phone', 'hours_worked', 'is_active']
+    list_filter = ['is_active']
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(is_superuser=False, is_staff=False)
+    
+    fieldsets = (
+        ('Thông tin tài khoản', {'fields': ('username', 'password')}),
+        ('Thông tin cá nhân', {'fields': ('name', 'age', 'email', 'phone', 'address',  'skills', 'hours_worked')}),
+        ('Quyền hạn', {'fields': ('is_active',)}),
+        ('Ngày đăng nhập cuối', {'fields': ('last_login',)}),
+    )
+    search_fields = ('username', 'name', 'age', 'email', 'phone', 'address', 'skills')
+    ordering = ('id',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'name', 'age', 'email', 'phone', 'address', 'skills', 'hours_worked', 'password1', 'password2', 'is_active')}
+        ),
+    )
+    filter_horizontal = ()
+        
+class AdminUserAdmin(UserAdmin):
+    list_display = ['username', 'name', 'email' ,'is_superuser', 'is_active']
+    list_filter = ['is_superuser', 'is_active']
+    search_fields = ['username', 'name', 'email']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(is_superuser=True)
+
     fieldsets = (
         ('Thông tin tài khoản', {'fields': ('username', 'password')}),
         ('Thông tin cá nhân', {'fields': ('name', 'age', 'email', 'phone', 'address',  'skills', 'hours_worked')}),
@@ -16,25 +82,40 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'name', 'age', 'email', 'phone', 'address', 'skills', 'hours_worked', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('username', 'name', 'age', 'email', 'phone', 'address', 'skills', 'hours_worked', 'password1', 'password2', 'is_staff', 'is_superuser' ,'is_active')}
         ),
     )
     filter_horizontal = ()
     
-class VolunteerEventPostAdmin(admin.ModelAdmin):
-    list_display = ['name', 'start_date', 'get_end_date', 'location', 'get_description', 'hours', 'status']
-    list_filter = ['start_date']
-    search_fields = ['name', 'location', 'description']
-    ordering = ['-start_date']
+class EventReportAdmin(admin.ModelAdmin):
+    list_display = ['event', 'report_date', 'participants_count', 'author']
+    list_filter = ['event', 'report_date']
+    search_fields = ['event__name', 'report_content']
     
-    def get_end_date(self, obj):
-        return obj.start_date + timedelta(days=obj.duration)
-    get_end_date.short_description = 'Ngày kết thúc'
+    fieldsets = (
+        (None, {
+            'fields': ('event', 'participants_count')
+        }),
+        ('Nội dung báo cáo', {
+            'fields': ('report_content',)
+        }),
+    )
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(event__organizer=request.user)
 
-    def get_description(self, obj):
-        return obj.description[:50]  
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'event':
+            kwargs['queryset'] = VolunteerEventPost.objects.filter(organizer=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-    get_description.short_description = 'Mô tả' 
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
+admin.site.register(EventReport, EventReportAdmin)
 admin.site.register(VolunteerEventPost, VolunteerEventPostAdmin)
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(AdminUser, AdminUserAdmin)
--- volunteer_management/volunteer_management_app/models.py
+++ volunteer_management/volunteer_management_app/models.py
@@ -10,17 +10,20 @@ from datetime import datetime
 from django.core.exceptions import ValidationError
 from django_ckeditor_5.fields import CKEditor5Field
 from datetime import timedelta
+from django.contrib.auth import get_user
 
 class VolunteerEventPost(models.Model):
     name = models.CharField(_('Tên sự kiện'), max_length=50, null=False, blank=False)
-    start_date = models.DateField(_('Ngày tổ chức'), null=False, blank=False)
-    duration = models.PositiveIntegerField(_('Thời hạn (ngày)'), default=10)
+    start_date = models.DateTimeField(_('Ngày tổ chức'), null=False, blank=False)
+    end_date = models.DateTimeField(_('Ngày kết thúc'), null=False, blank=False)
     location = models.CharField(_('Địa điểm'), max_length=100, null=False, blank=False)
-    description = CKEditor5Field(_('Mô tả'), null=False, blank=False, config_name='extends')
-    hours = models.IntegerField(_('Số giờ tình nguyện'), null=False, blank=False)
+    description = CKEditor5Field(_('Mô tả sự kiện'), null=False, blank=False, config_name='extends', help_text=_('Mô tả chi tiết về sự kiện, đây cũng sẽ là nội dụng  cho bài viết về sự kiện tình nguyện này.'))
+    hours = models.IntegerField(_('Số giờ tình nguyện'), null=False, blank=False, default=1, help_text=_('Số giờ tối thiểu mà người tham gia cần tình nguyện.'))
     likes = models.IntegerField(_('Lượt yêu thích'), default=0, editable=False)
     shares = models.IntegerField(_('Lượt chia sẻ'), default=0, editable=False)
-    participants = models.IntegerField(_('Số người tham gia'), default=0)
+    max_participants = models.IntegerField(_('Số người tham gia tối đa'), default=10)
+    current_participants = models.IntegerField(_('Số người tham gia hiện tại'), default=0, editable=False)
+    organizer = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='events_organized', verbose_name=_('Người tổ chức'), help_text=_('Chỉ người dùng quản trị mới có thể tổ chức sự kiện.'), null=False, blank=False)
     STATUS_CHOICES = [
         ('planned', _('Đang lên kế hoạch')),
         ('ongoing', _('Đang diễn ra')),
@@ -30,8 +33,24 @@ class VolunteerEventPost(models.Model):
         _('Trạng thái'), max_length=20, choices=STATUS_CHOICES, default='planned'
     )
     
-    def get_end_date(self):
-        return self.date + timedelta(days=self.duration)
+    def update_status(self):
+        if self.start_date > timezone.now():
+            self.status = 'planned'
+        elif self.start_date <= timezone.now() and self.get_end_date() >= timezone.now():
+            self.status = 'ongoing'
+        else:
+            self.status = 'completed'
+        self.save()
+        
+    def clean(self):
+        if self.hours < 1:
+            raise ValidationError(_('Số giờ tình nguyện phải lớn hơn hoặc bằng 1.'))
+        if self.start_date < timezone.now():
+            raise ValidationError(_('Ngày tổ chức không thể trước thời điểm hiện tại.'))
+        if self.start_date <= timezone.now():
+            raise ValidationError(_('Ngày tổ chức không được trùng với thời điểm hiện tại.'))
+        if self.end_date <= self.start_date:
+            raise ValidationError(_('Ngày kết thúc phải sau ngày bắt đầu.'))
     
     def __str__(self):
         return self.name
@@ -43,6 +62,8 @@ class VolunteerEventPost(models.Model):
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
         if not username:
             raise ValueError(_('Phải có tên đăng nhập'))
         username = self.model.normalize_username(username)
@@ -65,19 +86,19 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
                         null=False, blank=False
                         )
     is_superuser = models.BooleanField(
-        _('superuser status'),
+        _('Quản trị hệ thống'),
         default=False,
         help_text=_(
             'Chỉ định người dùng có tất cả các quyền quản trị hệ thống.'
         ), 
     )                                              
     is_staff = models.BooleanField(
-        _('staff status'),
+        _('Nhân viên hệ thống'),
         default=False,
         help_text=_('Chỉ định người dùng có quyền truy cập vào trang quản trị (admin).'),
     )
     is_active = models.BooleanField(
-        _('active'),
+        _('Trạng thái hoạt động'),
         default=True,
         help_text=_(
             'Chỉ định người dùng được coi là "active". Hủy chọn để vô hiệu hóa tài khoản này.'
@@ -88,23 +109,59 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     
     avatar = models.ImageField(_('Ảnh đại diện'), upload_to='avatars/', null=True, blank=True)
     name = models.CharField(_('Tên'), max_length=30, null=False, blank=False)
-    age = models.IntegerField(_('Tuổi'), null=False, blank=False)
+    age = models.IntegerField(_('Tuổi'), null=True, blank=True)
     email = models.EmailField(_('Email'), null=True, blank=True, unique=True)
     phone = models.CharField(_('Số điện thoại'), max_length=15, null=True, blank=True)
     address = models.CharField(_('Địa chỉ'), max_length=100, null=True, blank=True)
-    skills = models.CharField(_('Kỹ năng'), max_length=100, null=True, blank=True)
+    skills = models.CharField(_('Kỹ năng, sở thích'), max_length=100, null=True, blank=True)
     hours_worked = models.IntegerField(_('Số giờ tình nguyện'), default=0)
     liked_events = models.ManyToManyField(VolunteerEventPost, related_name='liked_users', blank=True, verbose_name=_('Sự kiện yêu thích'))
     events_attended = models.ManyToManyField(VolunteerEventPost, related_name='attendees', blank=True, verbose_name=_('Sự kiện đã tham gia'))
     
-    REQUIRED_FIELDS = ['name', 'age']
+    REQUIRED_FIELDS = ['name']
 
     def __str__(self):
-        return self.username
+        return self.name
     
     class Meta:
         verbose_name = _('Người dùng')
         verbose_name_plural = _('Người dùng')
-        ordering = ['username']
-        
+        ordering = ['name']
 
+class AdminUser(CustomUser):
+    class Meta:
+        proxy = True
+        verbose_name = 'Người dùng quản trị'
+        verbose_name_plural = 'Người dùng quản trị'
+        
+class EventReport(models.Model):
+    event = models.ForeignKey(VolunteerEventPost, on_delete=models.CASCADE, related_name='reports', verbose_name=_('Sự kiện'), help_text=_('Chỉ có thể báo cáo những sự kiện đã kết thúc mà đã bạn tổ chức.'), null=False, blank=False)
+    report_date = models.DateField(_('Ngày báo cáo'), auto_now_add=True)
+    participants_count = models.PositiveIntegerField(_('Số người tham gia'), default=0)
+    report_content = CKEditor5Field(_('Nội dung báo cáo'), config_name='extends')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reports', verbose_name=_('Người báo cáo'))
+       
+    class Meta:
+        verbose_name = _('Báo cáo sự kiện')
+        verbose_name_plural = _('Báo cáo sự kiện')
+        ordering = ['-report_date']
+        
+    def clean(self):
+        # if self.participants_count < 0:
+        #     raise ValidationError(_('Số người tham gia không thể âm.'))
+        # if self.participants_count > self.event.current_participants:
+        #     raise ValidationError(_('Số người tham gia không thể lớn hơn số người tham gia hiện tại của sự kiện.'))
+        if self.event.status != 'completed':
+            raise ValidationError(_('Chỉ có thể báo cáo khi sự kiện đã kết thúc.'))
+        if self.event.organizer != self.author:
+            raise ValidationError(_('Chỉ người tổ chức sự kiện mới có thể tạo báo cáo.'))
+        if self in self.event.reports.all():
+            raise ValidationError(_('Báo cáo này đã tồn tại.'))
+
+    def __str__(self):
+        return f"Báo cáo sự kiện '{self.event.name}' ngày {self.report_date}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # Nếu là báo cáo mới
+            self.author = self.event.organizer
+        super().save(*args, **kwargs)
